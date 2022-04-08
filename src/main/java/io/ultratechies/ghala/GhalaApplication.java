53c3033fca02fa2bacbe0ae8b664f264be57c770
Ian Njari <iannjari@gmail.com>
2022-04-08 09:54:19 +0300

    upload initial spring project files

---
+++ src/main/java/io/ultratechies/ghala/GhalaApplication.java
@@ -0,0 +1,13 @@
+package io.ultratechies.ghala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ultratechies/ghala/GhalaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ultratechies.ghala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
