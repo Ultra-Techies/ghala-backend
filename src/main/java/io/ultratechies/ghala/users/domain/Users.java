e1c0f55f7dfcd68bc41d74aeb2b8d40301090d97
Ian Njari <iannjari@gmail.com>
2022-04-11 17:41:29 +0300

    added user and role classes

---
+++ src/main/java/io/ultratechies/ghala/users/domain/Users.java
@@ -0,0 +1,35 @@
+package io.ultratechies.ghala.users.domain;
+
+import io.ultratechies.ghala.roles.domain.Roles;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+    @Id
+    @SequenceGenerator(
+            name = "users_sequence",
+            sequenceName = "users_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator="users_sequence"
+    )
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private String assignedWarehouse;
+    private String password;
+    @ManyToMany
+    private Collection<Roles> roles;
+}
