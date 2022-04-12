5457509cffe011916e533be506755faaa04756e8
Ian Njari <iannjari@gmail.com>
2022-04-12 12:09:23 +0300

    implemented warehouse rest and service

--- src/main/java/io/ultratechies/ghala/warehouse/service/WarehouseService.java
+++ src/main/java/io/ultratechies/ghala/warehouse/service/WarehouseService.java
@@ -4,9 +4,11 @@ import io.ultratechies.ghala.warehouse.domain.Warehouse;
 import io.ultratechies.ghala.warehouse.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,20 @@ public class WarehouseService {
     public List<Warehouse> getAllWarehouses(){
         return warehouseRepository.findAll();
 
+    }
+
+    public Optional<Warehouse> getWarehouseById(Long id){
+        return warehouseRepository.findById(id);
+    }
+
+    public ResponseEntity saveWarehouse(Warehouse warehouse){
+        warehouseRepository.save(warehouse);
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity deleteWarehouse(Long id){
+        warehouseRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
