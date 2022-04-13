8837c773196f8e9035f8060b4f547e361c0003d3
Ian Njari <iannjari@gmail.com>
2022-04-13 16:09:32 +0300

    added role functionality

---
+++ src/main/java/io/ultratechies/ghala/roles/rest/RolesResource.java
@@ -0,0 +1,26 @@
+package io.ultratechies.ghala.roles.rest;
+
+import io.ultratechies.ghala.roles.domain.Roles;
+import io.ultratechies.ghala.roles.service.RolesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/roles")
+public class RolesResource {
+    @Autowired
+    private final RolesService rolesService;
+
+    @GetMapping()
+    public List<Roles> getRoles(){
+        return rolesService.getAllRoles();
+    }
+}
