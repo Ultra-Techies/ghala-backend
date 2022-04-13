7e7d4b405c8ab81d29191c839858260eff81437c
Ian Njari <iannjari@gmail.com>
2022-04-13 16:06:09 +0300

    annotated repository

--- src/main/java/io/ultratechies/ghala/users/repository/UserRepository.java
+++ src/main/java/io/ultratechies/ghala/users/repository/UserRepository.java
@@ -3,9 +3,11 @@ package io.ultratechies.ghala.users.repository;
 import io.ultratechies.ghala.users.domain.Users;
 import io.ultratechies.ghala.warehouse.domain.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository <Users, Long>{
     Optional<Users> findUserById(Long id);
 }
