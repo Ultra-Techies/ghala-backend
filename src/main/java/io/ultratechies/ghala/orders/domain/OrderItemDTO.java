3e0d849a43479d6a80acb33887ff80eb362210c9
Ian Njari <iannjari@gmail.com>
2022-04-14 11:04:27 +0300

    changed OrderItemDTO to a JPA entity

--- src/main/java/io/ultratechies/ghala/orders/domain/OrderItemDTO.java
+++ src/main/java/io/ultratechies/ghala/orders/domain/OrderItemDTO.java
@@ -5,11 +5,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
+@Entity
 public class OrderItemDTO {
+    @Id
+    @SequenceGenerator(
+            name = "users_sequence",
+            sequenceName = "users_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator="users_sequence"
+    )
     private Long sku;
     private Integer quantity;
     private String name;
