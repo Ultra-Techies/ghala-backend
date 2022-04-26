c4693e4943b05ff44818099bbe5e3b639c6904d9
Ian Njari <iannjari@gmail.com>
2022-04-26 12:32:58 +0300

    Revert "OTP Feature"

--- src/main/java/io/ultratechies/ghala/GhalaApplication.java
+++ src/main/java/io/ultratechies/ghala/GhalaApplication.java
@@ -1,10 +1,6 @@
 package io.ultratechies.ghala;
 
-import com.twilio.Twilio;
 import io.ultratechies.ghala.config.SwaggerConfiguration;
-import io.ultratechies.ghala.config.TwilioConfiguration;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -13,23 +9,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableSwagger2
 @EnableWebSecurity
 @Import(SwaggerConfiguration.class)
 @ComponentScan("io.ultratechies.ghala")
-@RequiredArgsConstructor
 public class GhalaApplication {
-
-	@Autowired
-	private final TwilioConfiguration twilioConfiguration;
-
-	@PostConstruct
-	public void innitTwilio(){
-		Twilio.init(twilioConfiguration.getTwilioAccountSid(),twilioConfiguration.getTwilioAuthToken());
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GhalaApplication.class, args);
