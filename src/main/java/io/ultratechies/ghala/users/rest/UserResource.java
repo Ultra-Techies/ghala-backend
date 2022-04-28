e4374b7a20231b6774a762e1e10aa6a42d7c3cac
Ian Njari <iannjari@gmail.com>
2022-04-28 12:09:23 +0300

    addes cross origin

--- src/main/java/io/ultratechies/ghala/users/rest/UserResource.java
+++ src/main/java/io/ultratechies/ghala/users/rest/UserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
