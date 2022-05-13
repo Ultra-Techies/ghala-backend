2c34c5b445e9411c697aa71153eb983e01d690c7
Ian Njari <iannjari@gmail.com>
2022-05-13 17:55:09 +0300

    added cross-origin

--- src/main/java/io/ultratechies/ghala/security/rest/SecurityController.java
+++ src/main/java/io/ultratechies/ghala/security/rest/SecurityController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/refreshtoken")
 public class SecurityController {
