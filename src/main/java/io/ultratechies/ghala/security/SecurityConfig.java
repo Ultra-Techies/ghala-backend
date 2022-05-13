a49e3e912e52c66e5201495d65964e56ef51b1a4
Ian Njari <iannjari@gmail.com>
2022-05-13 14:12:25 +0300

    modified authorization

--- src/main/java/io/ultratechies/ghala/security/SecurityConfig.java
+++ src/main/java/io/ultratechies/ghala/security/SecurityConfig.java
@@ -34,30 +34,77 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/api/users","/refreshtoken").permitAll();
+
+        // permit all, non-secured
         http.authorizeRequests().antMatchers(POST,"/api/users").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/api/users/get/**").permitAll();
         http.authorizeRequests().antMatchers(POST,"/api/users/exists").permitAll();
-        http.authorizeRequests().antMatchers(POST,"/api/otp").permitAll();
         http.authorizeRequests().antMatchers(POST,"/api/order").permitAll();
-        http.authorizeRequests().antMatchers(GET,"/api/warehouse/**").permitAll();
-        http.authorizeRequests().antMatchers(GET,"/api/users").permitAll();//
-        http.authorizeRequests().antMatchers(PUT,"/api/users").permitAll();
-        http.authorizeRequests().antMatchers(DELETE,"/api/users").hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(POST,"/api/otp").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/refreshtoken/**").permitAll();
+
+        // users
+        http.authorizeRequests().antMatchers(DELETE,"/api/users/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers(GET,"/api/users/all").hasAnyAuthority("ADMIN","WH_MANAGER");
+        http.authorizeRequests().antMatchers(PUT,"/api/users")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","BASIC","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
         http.authorizeRequests().antMatchers(POST,"/api/users/fetch")
                 .hasAnyAuthority("ADMIN","WH_MANAGER","BASIC","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+
+        // warehouse
+        http.authorizeRequests().antMatchers(GET,"/api/warehouse/get/**")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","BASIC","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
         http.authorizeRequests().antMatchers(POST,"/api/warehouse")
+                .hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(GET,"/api/warehouse/all")
                 .hasAnyAuthority("ADMIN","SUPERVISOR");
-        http.authorizeRequests().antMatchers(DELETE,"/api/warehouse")
-                .hasAnyAuthority("ADMIN","SUPERVISOR");
+        http.authorizeRequests().antMatchers(DELETE,"/api/warehouse/**")
+                .hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers(PUT,"/api/warehouse")
-                .hasAnyAuthority("ADMIN","SUPERVISOR");
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR");
+
+        // orders
         http.authorizeRequests().antMatchers(GET,"/api/order/all")
-                .hasAnyAuthority("ADMIN","SUPERVISOR");
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
         http.authorizeRequests().antMatchers(GET,"/api/order/wh/**")
                 .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
-        http.authorizeRequests().antMatchers(GET,"/api/order")
-                .hasAnyAuthority("ADMIN","WH_MANAGER","BASIC","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(GET,"/api/order/get/**")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(DELETE,"/api/order")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(PUT,"/api/order")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+
+        // inventory
+        http.authorizeRequests().antMatchers(GET,"/api/inventory/all")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(GET,"/api/inventory/get/**")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(POST,"/api/inventory")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(PUT,"/api/inventory")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(DELETE,"/api/inventory")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR");
+        http.authorizeRequests().antMatchers(GET,"/api/inventory/wh/**")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+
+        // otp,not secured
+
+        // delivery notes
+        http.authorizeRequests().antMatchers(GET,"/api/deliverynotes/get/**")
+                .hasAnyAuthority("DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(POST,"/api/deliverynotes")
+                .hasAnyAuthority("DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(GET,"/api/deliverynotes/all")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+        http.authorizeRequests().antMatchers(PUT,"/api/deliverynotes")
+                .hasAnyAuthority("DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+
+        //stats
+        http.authorizeRequests().antMatchers(GET,"/api/stats/**")
+                .hasAnyAuthority("ADMIN","WH_MANAGER","SUPERVISOR","DISPATCH_ASSOCIATE","WH_ASSOCIATE");
+
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
